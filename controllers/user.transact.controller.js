4307b349e13f0ae7a4042b32662369e2df9d6035
DevPeter1454 <ojopeter36@gmail.com>
2022-08-31 09:50:10 +0100

    make it better

--- controllers/user.transact.controller.js
+++ controllers/user.transact.controller.js
@@ -21,7 +21,7 @@ const transact = (req, res) => {
 
 const getTransactions = (req, res) => {
     let allTransactions = [];
-    transactModel.find({senderAccount: req.body.senderAccount}, (err, form) => {
+    transactModel.find({}, (err, form) => {
         if(err){
             res.json({
                 message: 'Error in getting transactions',
@@ -30,32 +30,21 @@ const getTransactions = (req, res) => {
         }else{
             if(form.length > 0){
                 form.forEach(element => {
-                    allTransactions.push(element);
-                });
-                // res.json({
-                //     message: 'Transactions found',
-                //     transactions: allTransactions,
-                // });
-            }
-           transactModel.find({recieverAccount: req.body.receiverAccount}, (err, form) => {
-                if(err){
-                    res.json({
-                        message: 'Error in getting transactions',
-                        error: err.message,
-                    });
-                }else{
-                    if(form.length > 0){
-                        form.forEach(element => {
-                            allTransactions.push(element);
-                        });
-                        
+                    if(element.senderAccount == req.body.senderAccount || element.receiverAccount == req.body.receiverAccount){
+                        allTransactions.push(element);
                     }
-                }
-           });
-           res.json({
-            message: 'Transactions found',
-            transactions: allTransactions,
-        });
+                });         
+            }
+            if(allTransactions.length > 0){
+                res.json({
+                    message: 'Transactions found',
+                    transactions: allTransactions,
+                });
+            }else{
+                res.json({
+                    message: 'No transactions found',
+                });
+            }
         }
     });
     // transactModel.
